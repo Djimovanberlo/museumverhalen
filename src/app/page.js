24465a08b010ba17e46587e9500aa748c831944a
Djimovanberlo <djimovanberlo@gmail.com>
2025-05-16 17:44:53 +0200

    setup openAI

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
+import getOpenAiData from "@/services/openAI/client";
 import { css } from "../../styled-system/css";
 
-export default function Home() {
+const Home = async () => {
+  const data = await getOpenAiData({
+    instructions: "Generate a simple response",
+    input: "Hello",
+  });
+
+  console.log("Data", data);
   return (
     <div
       className={css({
@@ -9,6 +16,9 @@ export default function Home() {
       })}
     >
       Hello wurrold
+      <button>Get OpenAI Data</button>
     </div>
   );
-}
+};
+
+export default Home;
---
+++ src/services/openAI/client.js
@@ -0,0 +1,18 @@
+import OpenAI from "openai";
+
+const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY;
+
+const openAiClient = new OpenAI({
+  apiKey: OPEN_AI_API_KEY,
+});
+
+const getOpenAiData = async ({ instructions, input }) => {
+  const response = await openAiClient.responses.create({
+    model: "gpt-4o",
+    instructions,
+    input,
+  });
+  return response;
+};
+
+export default getOpenAiData;
